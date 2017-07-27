eba197081c518686c7f2dc585bce40bb43d431a8
张春雨 <18602650920@163.com>
2017-07-27 15:22:21 +0800

    修复badgeColor在10系统下无此api的问题

--- CYTabBar/CYTabBarController.m
+++ CYTabBar/CYTabBarController.m
@@ -7,6 +7,9 @@
 //
 
 #import "CYTabBarController.h"
+#if  __IPHONE_OS_VERSION_MAX_ALLOWED < __IPHONE_10_0
+#import "UITabBarItem+BadgeColor.h"
+#endif
 
 @interface CYTabBarController ()
 /** center button of place ( -1:none center button >=0:contain center button) */
